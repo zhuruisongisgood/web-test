b48d143faf1d80ce8ceadcef711244acda1bd45f
zhuruisong <ruisongzhu@huashenghaoche.com>
2019-12-20 15:10:19 +0800

    init

---
+++ src/main/java/com/example/webdemo/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.webdemo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/test/{name}")
+    public String test(@PathVariable String name){
+        return "welcome "+name;
+    }
+}
